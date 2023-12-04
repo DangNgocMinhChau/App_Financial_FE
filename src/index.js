03c987976e4bbd1f047d69b0bb81904fe1b21674
chaudang <chaudang1595@gmail.com>
2023-12-05 01:34:20 +0700

    chaudang update

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react"; // nạp thư viện react
+import ReactDOM from "react-dom"; // nạp thư viện react-dom
+import { DatePicker } from "antd";
+import { ConfigProvider } from "antd";
+// Tạo component App
+function App() {
+  return <h1>123123</h1>;
+}
+
+// Render component App vào #root element
+ReactDOM.render(
+    <App />,
+  document.getElementById("root")
+);
