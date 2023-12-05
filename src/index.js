b9021603221d7b35d10647a97c43d665966b9fc8
chaudang <chaudang1595@gmail.com>
2023-12-05 18:35:38 +0700

    update FE

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from "react"; // nạp thư viện react
+import React, { useId } from "react"; // nạp thư viện react
 import ReactDOM from "react-dom"; // nạp thư viện react-dom
 import { DatePicker } from "antd";
-import { ConfigProvider } from "antd";
+import "../css/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 // Tạo component App
 function App() {
-  return <h1>123123</h1>;
+  return <div><DatePicker />
+    <h1>chau dang</h1>
+    <div class="alert alert-dark" role="alert">
+      A simple dark alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
+    </div>
+  </div>;
 }
 
 // Render component App vào #root element
 ReactDOM.render(
-    <App />,
+  <App />,
   document.getElementById("root")
 );
